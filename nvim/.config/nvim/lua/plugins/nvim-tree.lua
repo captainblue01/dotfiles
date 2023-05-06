dd3f00f398c313700f48269c4db4bffc4663e2aa
BrodieH <brodie.henry99@gmail.com>
2023-05-06 16:49:08 +1000

    migrated nvim-tree keymaps to use on_attach

--- nvim/.config/nvim/lua/plugins/nvim-tree.lua
+++ nvim/.config/nvim/lua/plugins/nvim-tree.lua
@@ -1,5 +1,26 @@
 local m = { "nvim-tree/nvim-tree.lua", dependencies = { "nvim-tree/nvim-web-devicons" } }
 
+-- mappings = {
+--   custom_only = false,
+--   list = {
+--     { key = { "l", "<CR>", "o" }, cb = tree_cb("edit") },
+--     { key = "h",                  cb = tree_cb("close_node") },
+--     { key = "v",                  cb = tree_cb("vsplit") },
+--   },
+-- },
+local function on_attach(bufnr)
+  local api = require("nvim-tree.api")
+
+  local function opts(desc)
+    return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+  end
+  vim.keymap.set("n", "l", api.node.open.edit, opts("Open"))
+  vim.keymap.set("n", "o", api.node.open.edit, opts("Open"))
+  vim.keymap.set("n", "<CR>", api.node.open.edit, opts("Open"))
+  vim.keymap.set("n", "v", api.node.open.vertical,opts('Open: Vertical Split'))
+  vim.keymap.set("n", "h", api.node.open.horizontal,opts('Open: Horizontal Split'))
+end
+
 m.init = function()
   -- Disables netrw
   vim.g.loaded_netrw = 1
@@ -8,6 +29,7 @@ m.init = function()
   local nvim = require("nvim-tree")
   local tree_cb = require("nvim-tree.config").nvim_tree_callback
   nvim.setup({
+    on_attach = on_attach,
     disable_netrw = true,
     hijack_netrw = true,
     -- ignore_ft_on_setup = {
@@ -32,14 +54,6 @@ m.init = function()
       width = 30,
       hide_root_folder = false,
       side = "left",
-      mappings = {
-        custom_only = false,
-        list = {
-          { key = { "l", "<CR>", "o" }, cb = tree_cb("edit") },
-          { key = "h",                  cb = tree_cb("close_node") },
-          { key = "v",                  cb = tree_cb("vsplit") },
-        },
-      },
       number = false,
       relativenumber = false,
     },
