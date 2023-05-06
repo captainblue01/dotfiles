5c65a37be742a88ad31cfd52f94faac3b22dbed5
BrodieH <brodie.henry99@gmail.com>
2023-05-06 20:48:31 +1000

    cleaned up nvim tree config

--- nvim/.config/nvim/lua/plugins/nvim-tree.lua
+++ nvim/.config/nvim/lua/plugins/nvim-tree.lua
@@ -1,13 +1,5 @@
 local m = { "nvim-tree/nvim-tree.lua", dependencies = { "nvim-tree/nvim-web-devicons" } }
 
--- mappings = {
---   custom_only = false,
---   list = {
---     { key = { "l", "<CR>", "o" }, cb = tree_cb("edit") },
---     { key = "h",                  cb = tree_cb("close_node") },
---     { key = "v",                  cb = tree_cb("vsplit") },
---   },
--- },
 local function on_attach(bufnr)
   local api = require("nvim-tree.api")
 
@@ -17,58 +9,24 @@ local function on_attach(bufnr)
   vim.keymap.set("n", "l", api.node.open.edit, opts("Open"))
   vim.keymap.set("n", "o", api.node.open.edit, opts("Open"))
   vim.keymap.set("n", "<CR>", api.node.open.edit, opts("Open"))
-  vim.keymap.set("n", "v", api.node.open.vertical,opts('Open: Vertical Split'))
-  vim.keymap.set("n", "h", api.node.open.horizontal,opts('Open: Horizontal Split'))
+  vim.keymap.set("n", "v", api.node.open.vertical, opts("Open: Vertical Split"))
+  vim.keymap.set("n", "h", api.node.open.horizontal, opts("Open: Horizontal Split"))
 end
 
 m.init = function()
-  -- Disables netrw
+  -- disable netrw at the very start of your init.lua (strongly advised)
   vim.g.loaded_netrw = 1
   vim.g.loaded_netrwPlugin = 1
 
-  local nvim = require("nvim-tree")
-  local tree_cb = require("nvim-tree.config").nvim_tree_callback
-  nvim.setup({
+  require("nvim-tree").setup({
     on_attach = on_attach,
-    disable_netrw = true,
-    hijack_netrw = true,
-    -- ignore_ft_on_setup = {
-    --     "startify",
-    --     "dashboard",
-    --     "alpha",
-    -- },
-    open_on_tab = false,
-    hijack_cursor = false,
-    update_cwd = true,
-    sync_root_with_cwd = true,
-    respect_buf_cwd = true,
-    update_focused_file = {
-      enable = true,
-      update_root = false,
-    },
-    hijack_directories = {
-      enable = false,
-      auto_open = false,
-    },
+    sort_by = "case_sensitive",
     view = {
       width = 30,
-      hide_root_folder = false,
-      side = "left",
-      number = false,
-      relativenumber = false,
-    },
-    diagnostics = {
-      enable = true,
-      icons = {
-        hint = "",
-        info = "",
-        warning = "",
-        error = "",
-      },
     },
     renderer = {
+      group_empty = true,
       highlight_git = true,
-      root_folder_modifier = ":t",
       icons = {
         show = {
           file = true,
