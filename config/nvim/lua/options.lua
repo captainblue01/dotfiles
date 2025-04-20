b5f69689ea3fcae6c7ea5eee5c5a1c8bace5955e
Brodie H <brodie.henry99@gmail.com>
2025-04-20 22:26:07 +1000

    updated neovim config

--- config/nvim/lua/options.lua
+++ config/nvim/lua/options.lua
@@ -10,7 +10,7 @@ o.numberwidth = 4 -- set number column width to 2 {default 4}
 -- General Settings
 o.clipboard = "unnamedplus" -- allows neovim to access the system clipboard
 o.title = true
-o.cmdheight = 0 -- more space in the neovim command line for displaying messages
+o.cmdheight = 1 -- more space in the neovim command line for displaying messages
 o.completeopt = { "menuone", "noselect" } -- mostly just for cmp
 o.conceallevel = 0 -- so that `` is visible in markdown files
 o.fileencoding = "utf-8" -- the encoding written to a file
