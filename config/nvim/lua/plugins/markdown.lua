2603116ca5e702aae058bf5270b76ca3047e89fd
Brodie H <brodie.henry99@gmail.com>
2026-07-27 22:37:09 +1000

    added markdown preview to NVIM

---
+++ config/nvim/lua/plugins/markdown.lua
@@ -0,0 +1,7 @@
+local m = {
+	"MeanderingProgrammer/render-markdown.nvim",
+	opts = {},
+	dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" }, -- if you prefer nvim-web-devicons
+}
+
+return m
